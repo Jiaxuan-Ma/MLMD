edfbd98bb7b058e3ae7a7fa8b13090b09e893969
Mjx <599939317@qq.com>
2023-03-27 19:34:04 +0800

    delete results file

--- utils.py
+++ utils.py
@@ -85,11 +85,11 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-current_dir = os.path.abspath(os.path.dirname(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
-target_dir = os.path.join(parent_dir, 'results')
-if not os.path.exists(target_dir):
-    os.mkdir(target_dir)
+# current_dir = os.path.abspath(os.path.dirname(__file__))
+# parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+# target_dir = os.path.join(parent_dir, 'results')
+# if not os.path.exists(target_dir):
+#     os.mkdir(target_dir)
 
 
 # ====================== Page design ============================
