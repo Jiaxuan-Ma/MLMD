2fc7dc3365b4609ac3d7996465955aa2ce19dad5
Jiaxuan-Ma <599939317@qq.com>
2024-12-25 12:35:55 +0800

    update

--- MLMD.py
+++ MLMD.py
@@ -130,7 +130,7 @@ footer {visibility:hidden;}
 st.markdown(sysmenu,unsafe_allow_html=True)
 
 with st.sidebar:
-    select_option = option_menu("MLMD", ["Home Page", "Basic Data", "Feature Engineering","Cluster & ReduceDim", "Regression", "Classification", "Transfer Learning", "Model Inference","Surrogate Optimization","Active Learning","Interpretable Machine Learning"],
+    select_option = option_menu("Matdesign", ["Home Page", "Basic Data", "Feature Engineering","Cluster & ReduceDim", "Regression", "Classification", "Transfer Learning", "Model Inference","Surrogate Optimization","Active Learning","Interpretable Machine Learning"],
                     icons=['house', 'clipboard-data', 'menu-button-wide','circle','bezier2', 'subtract', 'arrow-repeat', 'app', 'microsoft','search','book-half'],
                     menu_icon="boxes", default_index=0)
     st.write('''
